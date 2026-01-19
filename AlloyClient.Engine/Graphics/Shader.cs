715ca715fc3b978913733fb72afbc2c1907d9f2c
Link <43941280+NotTheLegend@users.noreply.github.com>
2026-01-19 10:40:20 -0600

    rewrote texture/shader

---
+++ AlloyClient.Engine/Graphics/Shader.cs
@@ -0,0 +1,42 @@
+﻿namespace AlloyClient.Engine.Graphics;
+
+public sealed class Shader {
+
+    internal record struct UniformInfo(int Location, UniformType Type, int Size);
+
+    public readonly string Name;
+
+    private readonly int _handle;
+
+    private readonly Dictionary<string, UniformInfo> _uniforms = new();
+
+    public Shader(string path, (string, int)[] defines = null) {
+        Name = new DirectoryInfo(path).Name;;
+        _handle = GL.CreateProgram();
+        
+        ShaderHelper.Compile(_handle, path, defines);
+        ShaderHelper.LoadUniforms(_handle, _uniforms);
+    }
+
+    public void Apply() => GL.UseProgram(_handle);
+
+    public void SetValue(string uniform, Matrix4 matrix) => GL.ProgramUniformMatrix4f(_handle, GetLocation(uniform, UniformType.FloatMat4), 1, true, in matrix);
+
+    public void SetValue(string uniform, float value) => GL.ProgramUniform1f(_handle, GetLocation(uniform, UniformType.Float), value);
+
+    public void SetValue(string uniform, Vector2 value) => GL.ProgramUniform2f(_handle, GetLocation(uniform, UniformType.FloatVec2), 1, in value);
+
+    public void SetValue(string uniform, Texture texture) => GL.ProgramUniform1ui(_handle, GetLocation(uniform, UniformType.Sampler2d), texture.TextureUnit);
+
+    private int GetLocation(string uniform, UniformType type) {
+        if (!_uniforms.TryGetValue(uniform, out var info)) {
+            throw new Exception($"Unable to find uniform <{uniform}> in shader <{Name}>");
+        }
+
+        if (type != info.Type) {
+            throw new Exception($"Value does not match the type of uniform <{uniform}>");
+        }
+
+        return info.Location;
+    }
+}
---
+++ AlloyClient.Engine/Graphics/Texture.cs
@@ -0,0 +1,47 @@
+﻿using AlloyClient.Engine.Common;
+using ReFuel.Stb;
+
+namespace AlloyClient.Engine.Graphics;
+
+public sealed class Texture {
+    
+    public readonly int Width;
+    
+    public readonly int Height;
+    
+    internal uint TextureUnit;
+    
+    private readonly int _handle;
+
+    public Texture(string file) : this(File.ReadAllBytes(file)) { }
+
+    public Texture(ReadOnlySpan<byte> data) : this(StbImage.Load(data)) { }
+    
+    public Texture(StbImage image) : this(image.AsSpan<Color>(), image.Width, image.Height) { }
+    
+    public Texture(ReadOnlySpan<Color> data, int width, int height) {
+        Width = width;
+        Height = height;
+        _handle = GL.CreateTexture(TextureTarget.Texture2d);
+        
+        GL.TextureStorage2D(_handle, 1, SizedInternalFormat.Rgba8, width, height);
+        GL.TextureSubImage2D(_handle, 0, 0, 0, width, height, PixelFormat.Rgba, PixelType.UnsignedByte, data);
+    }
+
+    public void SetData(ReadOnlySpan<Color> data, Vector4i rect) => GL.TextureSubImage2D(_handle, 0, rect.X, rect.Y, rect.Z, rect.W, PixelFormat.Rgba, PixelType.UnsignedByte, data);
+
+    public void SetData(ReadOnlySpan<Color> data, int width, int height) => GL.TextureSubImage2D(_handle, 0, 0, 0, width, height, PixelFormat.Rgba, PixelType.UnsignedByte, data);
+    
+    public void SetFilter(TextureFilter filter) {
+        GL.TextureParameteri(_handle, TextureParameterName.TextureMagFilter, filter.MagFilter);
+        GL.TextureParameteri(_handle, TextureParameterName.TextureMinFilter, filter.MinFilter);
+    }
+
+    public void BindToTextureUnit(uint unit) {
+        if (unit > 15) {
+            throw new ArgumentOutOfRangeException(nameof(unit), unit, null);
+        }
+        
+        GL.BindTextureUnit(TextureUnit = unit, _handle);
+    }
+}
