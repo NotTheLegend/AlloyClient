e6964ef3080931be40a5559a56402610365a691c
Zolmex <zolmexyt@gmail.com>
2026-03-02 11:15:34 -0300

    Some particles effects courtesy of nekoT

--- AlloyClient/ParticleEffects/SparkEffect.cs
+++ AlloyClient/ParticleEffects/SparkEffect.cs
@@ -14,6 +14,14 @@ public class SparkEffect : ParticleEffect {
     private float _dx;
     private float _dy;
 
+    public SparkEffect(int size, uint color, int lifetime, float z, Vector2 start, Vector2 end) {
+        _startingSize = size;
+        _lifeTime = _timeLeft = lifetime;
+        _dx = (end.X - start.X) / (lifetime / 1000f);
+        _dy = (end.Y - start.Y) / (lifetime / 1000f);
+        _particle = new ParticleData(new Vector3(start, z), GetColor(color));
+    }
+
     public SparkEffect(int size, uint color, int lifetime, float z, float dx, float dy, float x, float y) {
         _startingSize = size;
         _lifeTime = _timeLeft = lifetime;
@@ -25,12 +33,12 @@ public class SparkEffect : ParticleEffect {
     public override bool Update(double time, double dt) {
         _timeLeft -= dt;
         if (_timeLeft <= 0) return false;
-        
-        var delta = (float)(dt / 1000d);
+
+        var delta = (float)(dt / 1000.0);
         _particle.Position.X += _dx * delta;
         _particle.Position.Y += _dy * delta;
         _particle.Position.W = (float)(_timeLeft / _lifeTime * _startingSize / 100d);
-        
+
         Map.AddParticle(_particle);
         return true;
     }
